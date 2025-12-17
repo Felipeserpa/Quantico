e8106fb9ebd762776173246d2e5d822c94ff4bde
Felipe serpa <felipe.mago_skt@hotmail.com>
2025-12-17 09:26:53 -0300

    implementaçao  aumento do cards

--- src/components/ImageSlide/ImageSlider.jsx
+++ src/components/ImageSlide/ImageSlider.jsx
@@ -29,7 +29,7 @@ const ImageSlider = ({ images }) => {
               style={{
                 width: '100%', // Tente ocupar a largura total do SwiperSlide
                 height: 'auto',
-                maxHeight: '200px' /* Mantenha a altura máxima */,
+                maxHeight: '300px' /* Mantenha a altura máxima card*/,
                 objectFit: 'cover',
                 display: 'block', // Importante para evitar espaços extras abaixo da imagem
               }}
